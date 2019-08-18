696f1d8c99ea11c8fcc94311819e7b51c1f1e5e5
codedump00 <blazinasis@gmail.com>
2019-08-18 15:52:36 +0545

    initialized validation sigini

--- lib/pages/Signin.dart
+++ lib/pages/Signin.dart
@@ -11,38 +11,47 @@ class SignInPage extends StatefulWidget {
 
 class _PageState extends State<SignInPage> {
   String email, password;
+
   @override
   Widget build(BuildContext context) {
+    var setEmail = (data) => setState(() {
+          data == email ? email = data : print('');
+        });
+    var setPassword = (data) => setState(() {
+          data == password ? password = data: print('');
+        });
     return SafeArea(
       child: Scaffold(
         resizeToAvoidBottomPadding: false,
         body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                    top: 5.0, left: 20.0, right: 20.0, bottom: 20.0),
-                child: Column(
-                  children: <Widget>[
-                    FForms(
-                        type: TextInputType.emailAddress,
-                        text: "Email",
-                        onChanged: (event) {}),
-                    SizedBox(height: 15.0),
-                    FForms(
-                        type: TextInputType.text,
-                        text: "Password",
-                        obscure: true,
-                        onChanged: (event) {}),
-                    SizedBox(height: 15.0),
-                    FRaisedButton(text: "Login", ),//onPressed: () {}),
-                    SizedBox(height: 30.0),
-                  ],
-                ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(
+                  top: 5.0, left: 20.0, right: 20.0, bottom: 20.0),
+              child: Column(
+                children: <Widget>[
+                  FForms(
+                      type: TextInputType.emailAddress,
+                      text: "Email",
+                      onChanged: setEmail),
+                  SizedBox(height: 15.0),
+                  FForms(
+                      type: TextInputType.text,
+                      text: "Password",
+                      obscure: true,
+                      onChanged: setPassword),
+                  SizedBox(height: 15.0),
+                  FRaisedButton(
+                    text: "Login",
+                  ), //onPressed: () {}),
+                  SizedBox(height: 30.0),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
