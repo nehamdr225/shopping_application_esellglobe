efc3502fb39aa07faffc661ce4bef983b3336ec5
nehamdr225 <nehamdr225@gmail.com>
2019-08-19 20:21:27 +0545

    changes

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,6 +1,6 @@
+import 'package:EsellGlobe/helpers/Api.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
-
 import '../widget/Carousel.dart';
 import '../widget/colors.dart';
 import '../widget/Icons.dart';
@@ -8,11 +8,24 @@ import './Drawer.dart';
 import '../widget/CategorySlider.dart';
 import '../widget/containers/Products.dart';
 
-class HomePageApp extends StatelessWidget {
+class HomePageApp extends StatefulWidget {
+  @override
+  _HomePageAppState createState() => _HomePageAppState();
+}
+
+class _HomePageAppState extends State<HomePageApp> {
+  List products;
+  @override
+  void initState() async{
+    super.initState();
+    products = await getProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     FlutterStatusbarcolor.setStatusBarColor(Colors.black87);
     var screenHeight = MediaQuery.of(context).size.height;
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.grey[200],
@@ -47,165 +60,36 @@ class HomePageApp extends StatelessWidget {
   }
 }
 
-final List<Map<String, dynamic>> products = [
-  {
-    "name": "Suit",
-    "picture": "images/1.jpg",
-    "price": "Rs. 1500",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 2800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Briefcase",
-    "picture": "images/2.png",
-    "price": "Rs. 1000",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1500",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shoes",
-    "picture": "images/3.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Track suit",
-    "picture": "images/4.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Track shoe",
-    "picture": "images/5.jpeg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "brief case",
-    "picture": "images/b2.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "T-Shirt",
-    "picture": "images/s1.jpg",
-    "price": "Rs. 500",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shoes",
-    "picture": "images/o1.jpg",
-    "price": "Rs. 1000",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1500",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "brief case",
-    "picture": "images/b1.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shirt",
-    "picture": "images/s4.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Brief case",
-    "picture": "images/b3.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shoes",
-    "picture": "images/o3.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "brief case",
-    "picture": "images/b1.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shirt",
-    "picture": "images/s4.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Brief case",
-    "picture": "images/b3.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shoes",
-    "picture": "images/o3.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "brief case",
-    "picture": "images/b1.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shirt",
-    "picture": "images/s4.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Brief case",
-    "picture": "images/b3.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  },
-  {
-    "name": "Shoes",
-    "picture": "images/o3.jpg",
-    "price": "Rs. 1300",
-    "seller": " eSellglobe",
-    "oldPrice": "Rs. 1800",
-    "details": " it is a very nice product"
-  }
-];
+class Products {
+  final String name;
+  final String description;
+  final String seller;
+  final int price;
+  final String category;
+  final int stock;
+  final int id;
+  final List media;
+
+  Products(
+      {this.name,
+      this.description,
+      this.category,
+      this.seller,
+      this.price,
+      this.id,
+      this.media,
+      this.stock});
+
+    factory Products.fromJson(Map< String, dynamic> json){
+      return Products(
+        category: json['category'],
+        description: json['description'],
+        id: json['id'],
+        media: json['media'],
+        name: json['name'],
+        price: json['price'],
+        seller: json['seller'],
+        stock: json['stock']
+      );
+    }
+}
