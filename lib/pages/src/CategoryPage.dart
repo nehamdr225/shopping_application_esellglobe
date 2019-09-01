8e1cd8dd8576f2a10ea846d570e10c3831e03ecb
nehamdr225 <nehamdr225@gmail.com>
2019-09-01 15:54:04 +0545

    category page grid view fix

--- lib/pages/src/CategoryPage.dart
+++ lib/pages/src/CategoryPage.dart
@@ -1,58 +1,63 @@
 import 'package:esell/state/state.dart';
 import 'package:esell/widget/atoms/Dropdown.dart';
 import 'package:esell/widget/atoms/Text.dart';
+import 'package:esell/widget/containers/ProductGrid.dart';
 // import 'package:esell/widget/containers/ProductGrid.dart';
 import 'package:esell/widget/molecules/AppBar.dart';
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 class CategoryPage extends StatelessWidget {
   final String text;
   CategoryPage({this.text});
   @override
   Widget build(BuildContext context) {
-    // final product = Provider.of<ProductModel>(context);
+    var height = MediaQuery.of(context).size.height;
+    var orientation = MediaQuery.of(context).orientation;
+    final product = Provider.of<ProductModel>(context);
     var cat = text == 'Top Wear'
         ? TOP
         : text == 'Bottom Wear'
             ? BOTTOM
             : text == "Foot Wear" ? FOOT : WATCH_GLASSES;
-    // var products = product.products;
+    var products = product.products;
     return SafeArea(
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(40.0),
-          child: FAppBar(wishlist: true, cart: true),
-        ),
-        body: Container(
-            child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+        child: Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(40.0),
+              child: FAppBar(wishlist: true, cart: true),
+            ),
+            body: Column(
               children: <Widget>[
-                FText(
-                  text: text,
-                  family: 'Ropa Sans',
-                  size: 18.0,
-                  padding: [15.0, 14.0],
-                  color: Colors.deepPurple[900],
+                Container(
+                  height: height*0.06,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      FText(
+                        text: text,
+                        family: 'Ropa Sans',
+                        size: 18.0,
+                        padding: [15.0, 14.0],
+                        color: Colors.deepPurple[900],
+                      ),
+                      DropDown(
+                        cap: cat[0],
+                        src: cat[1],
+                      )
+                    ],
+                  ),
                 ),
-                DropDown(
-                  cap: cat[0],
-                  src: cat[1],
+                Container(
+                  height: height*0.85,
+                  
+                  child: ProductGrid(
+                      orientation: orientation,
+                      products: products,
+                      count: product.count),
                 )
               ],
-            ),
-            // OrientationBuilder(builder: (context, orientation) {
-            //   return ProductGrid(
-            //       orientation: orientation,
-            //       products: products,
-            //       count: product.count);
-            // })
-          ],
-        )),
-      ),
-    );
+            )));
   }
 }
--- lib/widget/containers/ProductGrid.dart
+++ lib/widget/containers/ProductGrid.dart
@@ -9,30 +9,32 @@ class ProductGrid extends StatelessWidget {
   ProductGrid({this.orientation, this.products, this.count});
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: count,
-      scrollDirection: Axis.vertical,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-        childAspectRatio: 0.84,
-      ),
-      itemBuilder: (BuildContext context, int index) {
-        return products != null
-            ? Product(
-                name: products[index]['name'],
-                image: products[index]['media'][0]['src'].length > 0
-                    ? products[index]['media'][0]['src'][0]
-                    : null,
-                price: products[index]['price'],
-                seller: products[index]['seller'],
-                oldPrice: products[index]['oldPrice'],
-                details: products[index]['details'])
-            : CenterText(
-                text: 'Products are being loaded...',
-                size: 12.0,
-                indicator: true,
-              );
-      },
-    );
+    return OrientationBuilder(builder: (context, orientation) {
+      return GridView.builder(
+        itemCount: count,
+        scrollDirection: Axis.vertical,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+          childAspectRatio: 0.84,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return products != null
+              ? Product(
+                  name: products[index]['name'],
+                  image: products[index]['media'][0]['src'].length > 0
+                      ? products[index]['media'][0]['src'][0]
+                      : null,
+                  price: products[index]['price'],
+                  seller: products[index]['seller'],
+                  oldPrice: products[index]['oldPrice'],
+                  details: products[index]['details'])
+              : CenterText(
+                  text: 'Products are being loaded...',
+                  size: 12.0,
+                  indicator: true,
+                );
+        },
+      );
+    });
   }
 }
