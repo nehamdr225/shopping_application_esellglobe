0e392e0b917921494285e356eeafbdcf40086ec5
nehamdr225 <nehamdr225@gmail.com>
2020-01-07 01:17:54 +0545

    name email are assigned

--- lib/pages/AccountEdit.dart
+++ lib/pages/AccountEdit.dart
@@ -7,12 +7,25 @@ import 'package:esell/widget/atoms/Forms.dart';
 import 'package:esell/widget/atoms/RaisedButton.dart';
 import 'package:esell/widget/molecules/Icons.dart';
 import 'package:flutter/material.dart';
+import 'package:esell/state/state.dart';
+import 'package:provider/provider.dart';
 
-class AccountEdit extends StatelessWidget {
+class AccountEdit extends StatefulWidget {
+  @override
+  _AccountEditState createState() => _AccountEditState();
+}
+
+class _AccountEditState extends State<AccountEdit> {
+  List name = [];
+
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
+    final user = Provider.of<UserModel>(context);
+    final userData = user.user;
+    final token = user.token;
+
     return Scaffold(
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(40.0),
@@ -57,40 +70,101 @@ class AccountEdit extends StatelessWidget {
               CircleAvatar(
                 backgroundColor: Colors.white,
                 radius: 45.0,
-                child: Image.asset(
-                  'images/logo/logoonly1.png',
-                  height: 45.0,
-                  width: 45.0,
-                ),
+                child: userData['media'] != null
+                    ? Image.network(userData['media'])
+                    : Text(
+                        userData['name'].split(' ').reduce((a, b) {
+                          return '${a[0]} ${b[0]}';
+                        }),
+                        style: Theme.of(context).textTheme.body1.copyWith(
+                            color: primaryDark,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.w700),
+                      ),
               ),
             ],
           ),
           SizedBox(
             height: 10.0,
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: FForms(
-              underline: true,
-              text: 'First Name',
-              height: 75.0,
-              width: width * 0.90,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: FForms(
-              underline: true,
-              text: 'Last Name',
-              height: 75.0,
-              width: width * 0.90,
-            ),
-          ),
+          token != null
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'First Name',
+                          style: Theme.of(context)
+                              .textTheme
+                              .body1
+                              .copyWith(color: Colors.grey[500]),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        userData['name'].split(' ').reduce((a, b) {
+                          return '$a';
+                        }),
+                        style: Theme.of(context).textTheme.body1.copyWith(
+                            color: textColor,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    )
+                  ],
+                )
+              : Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: FForms(
+                    underline: true,
+                    text: 'First Name',
+                    height: 75.0,
+                    width: width * 0.90,
+                  ),
+                ),
+          token != null
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          'Last Name',
+                          style: Theme.of(context)
+                              .textTheme
+                              .body1
+                              .copyWith(color: Colors.grey[500]),
+                        )),
+                    Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          userData['name'].split(' ').reduce((a, b) {
+                            return '$b';
+                          }),
+                          style: Theme.of(context).textTheme.body1.copyWith(
+                              color: textColor,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w400),
+                        )),
+                  ],
+                )
+              : Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: FForms(
+                    underline: true,
+                    text: 'Last Name',
+                    height: 75.0,
+                    width: width * 0.90,
+                  ),
+                ),
           FRaisedButton(
             text: 'SUBMIT',
             color: primaryDark,
-            bg:  Colors.white,
-            onPressed: (){
+            bg: Colors.white,
+            onPressed: () {
               // Navigator.push(context,
               //           MaterialPageRoute(builder: (context) => ChangePassword()));
             },
--- lib/pages/AccountPage.dart
+++ lib/pages/AccountPage.dart
@@ -10,11 +10,17 @@ import 'package:esell/widget/molecules/AccountCards.dart';
 import 'package:esell/widget/molecules/Icons.dart';
 import 'package:flutter/material.dart';
 
+import 'package:esell/state/state.dart';
+import 'package:provider/provider.dart';
+
 class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
+    final user = Provider.of<UserModel>(context);
+    final userData = user.user;
+    //final token = user.token;
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: PreferredSize(
@@ -49,7 +55,8 @@ class AccountPage extends StatelessWidget {
           )),
       body: ListView(
         children: <Widget>[
-          Stack(// Blue name card
+          Stack(
+            // Blue name card
             alignment: AlignmentDirectional.center,
             children: <Widget>[
               Container(
@@ -64,29 +71,37 @@ class AccountPage extends StatelessWidget {
                   CircleAvatar(
                     backgroundColor: Colors.white,
                     radius: 45.0,
-                    child: Image.asset(
-                      'images/logo/logoonly1.png',
-                      height: 45.0,
-                      width: 45.0,
-                    ),
+                    child: userData['media'] != null
+                        ? Image.network(userData['media'])
+                        : Text(
+                            userData['name'].split(' ').reduce((a, b) {
+                              return '${a[0]} ${b[0]}';
+                            }),
+                            style: Theme.of(context)
+                                .textTheme
+                                .body1
+                                .copyWith(color: primaryDark, fontSize: 24.0, fontWeight: FontWeight.w700),
+                          ),
                   ),
-                  Padding(padding: EdgeInsets.all(5.0),),
-                  Text('Name',
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                  ),
+                  Text(userData['name'],
                       style: Theme.of(context).textTheme.title.copyWith(
                           color: Colors.white,
                           fontSize: 20,
                           fontWeight: FontWeight.w600)),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       Container(
                         width: width * 0.55,
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: <Widget>[
-                            Text('Email',
+                            Text(userData['email'],
                                 style: Theme.of(context)
                                     .textTheme
                                     .title
@@ -95,18 +110,25 @@ class AccountPage extends StatelessWidget {
                                         fontSize: 14,
                                         fontWeight: FontWeight.w500)),
                             //SizedBox(width: width * 0.32,),
-                            IconButton(
-                              icon: Icon(Icons.edit),
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => AccountEdit()));
-                              },
-                            )
                           ],
                         ),
                       ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => AccountEdit()));
+                                },
+                              ),
+                        ],
+                      )
+                     
+                      
                     ],
                   ),
                 ],
@@ -150,48 +172,49 @@ class AccountPage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => AddressPage()));
             },
           ),
-          SizedBox(height: 10.0,),
+          SizedBox(
+            height: 10.0,
+          ),
           FRaisedButton(
             text: 'Language Settings',
             color: textColor,
-            bg:  Colors.white,
-            onPressed: (){},
+            bg: Colors.white,
+            onPressed: () {},
             width: width * 0.90,
             height: 50.0,
           ),
           FRaisedButton(
             text: 'Notification Preferences',
             color: textColor,
-            bg:  Colors.white,
-            onPressed: (){},
+            bg: Colors.white,
+            onPressed: () {},
             width: width * 0.90,
             height: 50.0,
           ),
           FRaisedButton(
             text: 'Account Settings',
             color: textColor,
-            bg:  Colors.white,
-            onPressed: (){},
+            bg: Colors.white,
+            onPressed: () {},
             width: width * 0.90,
             height: 50.0,
           ),
           FRaisedButton(
             text: 'Logout of this app',
             color: textColor,
-            bg:  Colors.white,
-            onPressed: (){},
+            bg: Colors.white,
+            onPressed: () {},
             width: width * 0.90,
             height: 50.0,
           ),
           FRaisedButton(
             text: 'Logout of all devices',
             color: textColor,
-            bg:  Colors.white,
-            onPressed: (){},
+            bg: Colors.white,
+            onPressed: () {},
             width: width * 0.90,
             height: 50.0,
           ),
-
         ],
       ),
     );
