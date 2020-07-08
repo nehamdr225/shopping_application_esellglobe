80945d17fc32aab176f7e4f1d7392914094ec0f7
codedump00 <blazinasis@gmail.com>
2020-07-08 14:00:04 +0545

    cleanup

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/nae/flutter"
-export "FLUTTER_APPLICATION_PATH=/run/media/nae/workshop/LightWebGroup/reesell/esellglobe"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\Users\Nct\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter\esellglobe"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/nae/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.6"
-export "FLUTTER_BUILD_NUMBER=6"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Nct\Documents\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.7"
+export "FLUTTER_BUILD_NUMBER=7"
